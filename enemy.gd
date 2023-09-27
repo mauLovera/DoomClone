c8cdacf224bc5b693fdcb4a6e8289dfbfb39ffc7
mauLovera <mauricioloveraedu@gmail.com>
2023-09-26 21:55:56 -0400

    feat: initial commit

---
+++ enemy.gd
@@ -0,0 +1,41 @@
+extends CharacterBody3D
+
+@onready var animated_sprite_3d = $AnimatedSprite3D
+@onready var player : CharacterBody3D = get_tree().get_first_node_in_group("player")
+
+@export var move_speed = 2.0
+@export var attack_range = 2.0
+
+var is_dead = false 
+
+func _physics_process(delta):
+	if is_dead: 
+		return
+	if player == null:
+		return
+	
+	var direction = player.global_position - global_position
+	direction.y = 0.0
+	direction = direction.normalized()
+	
+	velocity = direction * move_speed
+	move_and_slide()
+	attempt_to_attack_player()
+	
+func attempt_to_attack_player():
+	var distance_to_player = global_position.distance_to(player.global_position)
+	if distance_to_player > attack_range:
+		return
+		
+	var eye_line = Vector3.UP * 1.5
+	var raycast_query = PhysicsRayQueryParameters3D.create(global_position + eye_line, player.global_position + eye_line, 1)
+	
+	var result = get_world_3d().direct_space_state.intersect_ray(raycast_query)
+	if result.is_empty():
+		player.kill()
+	
+func kill():
+	is_dead = true
+	$DeathSound.play()
+	animated_sprite_3d.play("death")
+	$CollisionShape3D.disabled = true
---
+++ player.gd
@@ -0,0 +1,77 @@
+extends CharacterBody3D
+
+@onready var animated_sprite_2d = $CanvasLayer/GunBase/AnimatedSprite2D
+@onready var ray_cast_3d = $RayCast3D
+@onready var shoot_sound = $ShootSound
+@onready var death_sound = $PlayerDeath
+
+const SPEED = 5.0
+const MOUSE_SENSITIVITY = 0.3
+
+var can_shoot = true
+var is_dead = false
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	animated_sprite_2d.animation_finished.connect(shoot_animation_is_done)
+	$CanvasLayer/DeathScreen/Panel/Button.button_up.connect(restart)
+	
+func _input(event):
+	if is_dead:
+		return
+	if event is InputEventMouseMotion:
+		rotation_degrees.y -= event.relative.x * MOUSE_SENSITIVITY
+
+func _process(delta):
+	if Input.is_action_just_pressed("exit"):
+		get_tree().quit()
+	if Input.is_action_just_pressed("restart"):
+		restart()
+		
+	if is_dead:
+		return
+	
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+
+func _physics_process(delta):
+	if is_dead:
+		return
+		
+	var input_direction = Input.get_vector("move_left", "move_right", "move_forwards", "move_backwards")
+	var direction = (transform.basis * Vector3(input_direction.x, 0, input_direction.y)).normalized()
+	
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+		
+	move_and_slide()
+
+func restart():
+	get_tree().reload_current_scene()
+	
+func shoot():
+	if !can_shoot:
+		return
+	can_shoot = false
+	animated_sprite_2d.play("shoot")
+	shoot_sound.play()
+	
+	if ray_cast_3d.is_colliding() and ray_cast_3d.get_collider().has_method("kill"):
+		ray_cast_3d.get_collider().kill()
+	
+func shoot_animation_is_done():
+	can_shoot = true
+	
+func kill():
+	if is_dead:
+		return
+		
+	is_dead = true
+	death_sound.play()
+	$CanvasLayer/DeathScreen.show()
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
